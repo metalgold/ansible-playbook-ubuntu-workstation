e62af1c3b3031d76584a676fb34c4317c6bf1825
lastcosmonaut <lastcosmonaut@me.com>
2022-03-23 08:01:03 +0100

    Create wsl-ansible.sh

---
+++ wsl-ansible.sh
@@ -0,0 +1,7 @@
+sudo ansible-galaxy install -r requirements.yml
+ansible-galaxy install -r requirements.yml
+
+sudo ansible-galaxy collection install -r requirements.yml
+ansible-galaxy collection install -r requirements.yml
+
+ansible-playbook wsl-playbook.yml -K
